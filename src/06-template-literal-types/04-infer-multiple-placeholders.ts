84aaeb02f8cc871b6bc3f2b37a6dded72cfea532
Nils Knappmeier <nils.knappmier@cosee.biz>
2021-09-29 01:26:57 +0200

    add example

--- src/06-template-literal-types/04-infer-multiple-placeholders.ts
+++ src/06-template-literal-types/04-infer-multiple-placeholders.ts
@@ -8,3 +8,11 @@ type ExtractParams<T> = T extends `${string}{${infer Param}}${infer Rest}`
   : never;
 
 const param1: ExtractParams<"Hello {you}, my name is {me}?"> = "me";
+
+// The following types are all equivalent
+type Step1 = ExtractParams<"Hello {you}, my name is {me}?">
+type Step2 = 'you' | ExtractParams<", my name is {me}?">
+type Step3 = 'you' | 'me' | ExtractParams<"?">
+type Step4 = 'you' | 'me' | never
+type Step5 = 'you' | 'me'
+
